758cdfb05866d3ba28435823d8970249556f0742
Anudeep Madrampalli <anudeep571@gmail.com>
2025-07-16 17:43:56 +0530

    init

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-/*
+
   cloud {
     organization = "anuworks"
 
@@ -7,7 +7,7 @@ terraform {
       name = "learn-terraform"
     }
   }
-*/
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
